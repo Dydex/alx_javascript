1d8952c71ae6c2932a52f6df46a5ca5c9047fa59
Ajala <supremeajala@gmail.com>
2023-09-06 17:41:09 +0100

    last task

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,18 +1,16 @@
 //  This script adds a new function 'incr' that increments the integer value
 const myObject = {
-    type: 'object'
+    type: 'object',
+    value: 12,
+    incr: function () {
+        this.value++;
+    },
 };
+
 console.log(myObject);
-
-function incr() {
-    this.value++;
-};
-
-myObject.incr = incr;
 myObject.incr();
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
-
