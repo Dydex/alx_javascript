c52808cd1fe13ea5ad4922861025044b0fbfb94e
Ajala <supremeajala@gmail.com>
2023-09-06 16:22:04 +0100

    sixth task

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,12 +1,7 @@
 // This script contains a function that executes x times a function
-function callMeMoby(x, theFunction) {
+function executeXTimes(x, theFunction) {
     for (let i = 0; i < x; i++) {
         theFunction();
     }
 }
 
-function theFunction() {
-    console.log("Where I am!");
-}
-
-callMeMoby(x, theFunction);
