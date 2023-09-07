542890569e120ccbd1db16f145753ca60239aeba
Ajala <supremeajala@gmail.com>
2023-09-07 11:30:53 +0100

    second task

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -3,12 +3,10 @@ class Rectangle {
     constructor(w, h) {
         this.width = w;
         this.height = h;
-    };
+        if (w = number || h <= number);
+    }
 }
 const r1 = new Rectangle(w, h);
-console.log(r1);
-console.log(r1.constructor);
-console.log(r1.width);
-console.log(r1.height);
+
 
 module.exports = Rectangle;
