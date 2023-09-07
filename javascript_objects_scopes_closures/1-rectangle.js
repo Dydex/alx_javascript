0f86d512bdf485282cd67c6f9dd6867bd64108a6
Ajala <supremeajala@gmail.com>
2023-09-07 10:46:13 +0100

    second task

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+// This script contains a class Rectangle 
+class Rectangle {
+    constructor(w, h) {
+        this.width = w;
+        this.height = h;
+    };
+}
+const r1 = new Rectangle();
+console.log(r1.)
+console.log(r1.width)
+console.log(r1.height)
