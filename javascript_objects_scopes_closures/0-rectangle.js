75b954148e2cfeac3b05ec0451c8c0b12e6acab3
Ajala <supremeajala@gmail.com>
2023-09-07 10:39:22 +0100

    first task

--- javascript_objects_scopes_closures/0-rectangle.js
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -1,8 +1,9 @@
 // This script contains an empty class Rectangle 
 class Rectangle {
-    constructor(Rectangle) {
-        this.Rectangle = Rectangle
+    constructor() {
+        
     };
 }
 const r1 = new Rectangle();
-console.log(r1)
+console.log(r1);
+console.log(r1.constructor);
