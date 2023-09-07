a75bb1c563c3fb3a089dd97484218195f360411e
Ajala <supremeajala@gmail.com>
2023-09-07 11:55:34 +0100

    third task

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+// This script contains a class Rectangle 
+class Rectangle {
+    constructor(w, h) {
+    if (w > 0 && h > 0) {
+        this.width = w;
+        this.height = h;
+    } else{
+        return {}
+        }
+        
+        }
+}
+
+module.exports = Rectangle;
