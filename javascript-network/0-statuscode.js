242173d6ffb65889c29da664bdd7de931dfa4f02
Ajala <supremeajala@gmail.com>
2023-09-11 21:06:01 +0100

    first task

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -10,7 +10,7 @@ request.get(url, function(error, response) {
     if (error) {
         console.error('Error:', error);
     } else {
-        console.log('code: ${response.statusCode}');
+        console.log(`code: ${response.statusCode}`);
     }
 });
 
