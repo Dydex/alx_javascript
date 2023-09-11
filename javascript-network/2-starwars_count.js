2256a83027c574e924cdb0ed9cc6b59e02a6ac21
Ajala <supremeajala@gmail.com>
2023-09-12 00:26:59 +0100

    third task

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -3,11 +3,9 @@
 
 const request = require('request');
 
-// Get the API URL from the command line arguments
-const apiUrl = process.argv[2];
-
-// Character ID for wedge antilles
-const characterId = 18;
+// Get the API URL from the command line arguments or use the default URL
+const apiUrl = process.argv[2] || 'https://swapi-api.alx-tools.com/api/films';
+const characterId = 18; // Character ID for Wedge Antilles
 
 // Make a GET request to the API endpoint
 request.get(apiUrl, function (error, response, body) {
@@ -16,15 +14,15 @@ request.get(apiUrl, function (error, response, body) {
     } else if (response.statusCode !== 200) {
         console.error('Request failed with status code:', response.statusCode);
     } else {
-        try{
-            // parse the JSON response
+        try {
+            // Parse the JSON response
             const filmData = JSON.parse(body);
 
             // Filter the films where Wedge Antilles is present
             const filmsWithWedge = filmData.results.filter(function (film) {
                 return film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`);
             });
-        
+
             // Display the count of films with Wedge Antilles
             console.log(filmsWithWedge.length);
         } catch (parseError) {
