d861aa5d6b45a1da9704832a100757b0b79957d4
Ajala <supremeajala@gmail.com>
2023-09-14 18:15:18 +0100

    fourth task

--- Javascript_advanced/2-function_me.js
+++ Javascript_advanced/2-function_me.js
@@ -5,7 +5,7 @@ function welcomeMessage(fullName) {
     return function() {
         alert('Welcome' + ' ' + fullName)
     }
-}
+};
 
 let guillaume = welcomeMessage('Guillaume');
 let alex = welcomeMessage('Alex');
